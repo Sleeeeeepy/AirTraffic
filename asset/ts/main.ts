24ddca1d90b56863cf0340a414f19298a7edc119
Sleeeeeepy <adjustedmin@gmail.com>
2021-10-30 17:51:42 +0900

    dot on earth

--- asset/ts/main.ts
+++ asset/ts/main.ts
@@ -1,5 +1,6 @@
 import { Buffer } from './TD/Buffer.js';
 import { Camera } from './TD/Camera.js';
+import { DataProvider } from './TD/DataProvider.js';
 import { Earth } from './TD/Earth.js';
 import { GL } from './TD/GL.js';
 import { Shader } from './TD/Shader.js';
@@ -22,13 +23,12 @@ function main() {
     let fshader = new Shader("earth.frag", gl.FRAGMENT_SHADER);
 
     // 셰이더 프로그램 생성
-    let prog = new ShaderProgram("shader");
-    let cprog = prog.getShaderProgram(vshader, fshader);
+    let prog = new ShaderProgram("earth", vshader, fshader);
     prog.use();
 
     // 버퍼에 정점 정보를 입력합니다.
     let vertexBuffer = new Buffer(gl.ARRAY_BUFFER, gl.STATIC_DRAW);
-    vertexBuffer.upload(new Float32Array(Earth.vertice));
+    vertexBuffer.upload(new Float32Array(Earth.vertex));
     vertexBuffer.unbind();
 
     // 버퍼에 인덱스 정보를 입력합니다.
@@ -43,16 +43,12 @@ function main() {
 
     // 정점 설정
     vertexBuffer.bind();
-    let vertexPositionAttribute = gl.getAttribLocation(cprog, "vPosition");
-    gl.enableVertexAttribArray(vertexPositionAttribute);
-    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+    prog.setVertexArrayObject("vPosition", vertexBuffer, 3, gl.FLOAT, false, 0, 0);
     vertexBuffer.unbind();
 
     // 텍스처 좌표 설정
     uvBuffer.bind();
-    let uvAttrb = prog.setAttribute("vinTexturecoord");
-    gl.enableVertexAttribArray(uvAttrb);
-    gl.vertexAttribPointer(uvAttrb, 2, gl.FLOAT, false, 0, 0);
+    prog.setVertexArrayObject("vinTexturecoord", uvBuffer, 2, gl.FLOAT, false, 0, 0);
     uvBuffer.unbind();
 
     //텍스쳐 설정
@@ -65,11 +61,11 @@ function main() {
 
     // 카메라 설정
     let orbitRadius = 100;
-    let zoom = 0.5;
-    let pov = Math.PI / 3
+    let zoom = 0.7;
+    let fov = Math.PI / 3
     let aspect = 1.0;
-    let camera = new Camera(pov, aspect, 0.1, 2000);
-    function initCamera(radian: number, axis: vec3): mat4 {
+    let camera = new Camera(fov, aspect, 0.1, 2000);
+    function initCamera(radian: number, axis: vec3, prog: ShaderProgram): mat4 {
         let cameraMatrix = new mat4([
             1, 0, 0, 0,
             0, 1, 0, 0,
@@ -87,45 +83,72 @@ function main() {
 
     function camRotateZ(cameraMatrix: mat4, radian: number) {
         cameraMatrix.rotate(radian, new vec3([0, 0, 1]));
-        let uict = prog.getUniformLocation("uCameraMatrix");
-        gl.uniformMatrix4fv(uict, false, cameraMatrix.all());
     }
 
     function camRotateX(cameraMatrix: mat4, radian: number) {
-        cameraMatrix.rotate(radian, new vec3([1, 0, 0]));
-        let uict = prog.getUniformLocation("uCameraMatrix");
-        gl.uniformMatrix4fv(uict, false, cameraMatrix.all());
+        cameraMatrix.rotate(radian, new vec3([0, 1, 0]));
     }
-    let cam = initCamera(Math.PI / 2, new vec3([1, 0, 0]));
-    draw();
 
+    let cam = initCamera(Math.PI / 2, new vec3([1, 0, 0]), prog);
+    drawEarth(indexBuffer, prog);
+
+    let fvshader = new Shader("flight.vert", gl.VERTEX_SHADER);
+    let ffshader = new Shader("flight.frag", gl.FRAGMENT_SHADER);
+    let fprog = new ShaderProgram("flight", fvshader, ffshader);
+    fprog.use();
+    
+    let fvbo = new Buffer(gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+    let melbourne = Earth.pointAt(1.01, 144, -37);
+    let seoul = Earth.pointAt(1.01, 126, 37);
+    let tokyo = Earth.pointAt(1.01, 139, 35);
+    let newyork = Earth.pointAt(1.01, -73, 40);
+    let origin = Earth.pointAt(1.01, 0, 0);
+    let points = melbourne.concat(seoul, tokyo, newyork, origin);
+    console.log(points);
+    fvbo.upload(new Float32Array(points));
+    fvbo.bind();
+    
+    let uict = fprog.getUniformLocation("uCameraMatrix");
+    gl.uniformMatrix4fv(uict, false, cam.all());
+    
+    let ptr = fprog.getAttributeLocation("vPosition");
+    gl.enableVertexAttribArray(ptr);
+    gl.vertexAttribPointer(ptr, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.POINTS, 0, 5);
+    
     //카메라를 회전시키는 임시 코드
     var tick = setInterval(function () {
-        camRotateZ(cam, Math.PI / 720);
-        camRotateX(cam, -Math.PI / 360);
-        draw();
-    }, 10);
-    /*
-        let points = Earth.pointAt(1.0, 23.3, 22.8);
-        let buffer2 = new Buffer(gl.ARRAY_BUFFER, gl.STATIC_DRAW);
-        buffer2.upload(new Float32Array(points));
+        camRotateZ(cam, Math.PI / 100);
+        //camRotateX(cam, -Math.PI / 360);
+        let uict = fprog.getUniformLocation("uCameraMatrix");
+        gl.uniformMatrix4fv(uict, false, cam.all());
+        uict = prog.getUniformLocation("uCameraMatrix");
+        gl.uniformMatrix4fv(uict, false, cam.all());
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        drawEarth(indexBuffer, prog);
+        drawPoint(fvbo, fprog);
+    }, 100);
     
-        gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-        gl.drawArrays(gl.POINTS, 0, 1);
-        gl.flush();
-    */
 }
 
 function initGL(gl: WebGLRenderingContext) {
     //gl.viewport(0, 0, canvas.width, canvas.height);
     gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
 
-function draw() {
+function drawEarth(buffer: Buffer, program: ShaderProgram) {
     const gl = GL.instance;
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    program.use();
+    buffer.bind();
     gl.drawElements(gl.TRIANGLE_STRIP, Earth.index.length, gl.UNSIGNED_SHORT, 0);
-    gl.flush();
+}
+
+function drawPoint(buffer: Buffer, program: ShaderProgram) {
+    const gl = GL.instance;
+    program.use();
+    buffer.bind();
+    gl.drawArrays(gl.POINTS, 0, 5);
 }
