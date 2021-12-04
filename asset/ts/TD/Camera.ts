abef657fdb4965cb2d5fcb0940d8284cb1d7dea7
Sleeeeeepy <adjustedmin@gmail.com>
2021-12-05 02:46:38 +0900

    Use quaternion to rotate camera

--- asset/ts/TD/Camera.ts
+++ asset/ts/TD/Camera.ts
@@ -18,6 +18,7 @@ export class Camera {
     private _up: vec3 = new vec3([0, 1, 0]);
     private isUpdated: boolean = false;
     private _adjustFar = false;
+    private _quat: quat = new quat([0, 0, 0, 1]);
     private static readonly _xAxis: vec3 = new vec3([1, 0, 0]);
     private static readonly _yAxis: vec3 = new vec3([0, 1, 0]);
     private static readonly _zAxis: vec3 = new vec3([0, 0, 1]);
@@ -158,21 +159,48 @@ export class Camera {
         this.isUpdated = true;
     }
 
+    // rotate with X axis
     public RotateX(radian: number) {
         if (radian == 0) return;
         this._worldMatrix.rotate(radian, Camera._xAxis);
         this.isUpdated = true;
     }
 
+    // rotate with Y axis
     public RotateY(radian: number) {
         if (radian == 0) return;
         this._worldMatrix.rotate(radian, Camera._yAxis);
         this.isUpdated = true;
     }
 
+    // rotate with Z axis
     public RotateZ(radian: number) {
         if (radian == 0) return;
         this._worldMatrix.rotate(radian, Camera._zAxis);
         this.isUpdated = true;
     }
+
+    // rotate using quaternion with euler angle.
+    // x->y->z
+    public RotateQuatEuler(Xradian: number, Yradian: number, Zradian: number) {
+        Xradian /= 2;
+        Yradian /= 2;
+        Zradian /= 2;
+        let x = Math.sin(Xradian) * Math.cos(Yradian) * Math.cos(Zradian) + Math.cos(Xradian) * Math.sin(Yradian) * Math.sin(Zradian);
+        let y = Math.cos(Xradian) * Math.sin(Yradian) * Math.cos(Zradian) - Math.sin(Xradian) * Math.cos(Yradian) * Math.sin(Zradian);
+        let z = Math.cos(Xradian) * Math.cos(Yradian) * Math.sin(Zradian) + Math.sin(Xradian) * Math.sin(Yradian) * Math.cos(Zradian);
+        let w = Math.cos(Xradian) * Math.cos(Yradian) * Math.cos(Zradian) - Math.sin(Xradian) * Math.sin(Yradian) * Math.sin(Zradian);
+        this._quat = new quat([x, y, z, w]);
+        this._worldMatrix = this._quat.toMat4().multiply(this.worldMatrix.copy());
+    }
+    /*
+    public RotateQuat(axis: vec3, radian: number) {
+        let x = axis.x * Math.sin(radian / 2);
+        let y = axis.y * Math.sin(radian / 2);
+        let z = axis.z * Math.sin(radian / 2);
+        let w = Math.cos(radian / 2);
+        this._quat = new quat([x, y, z, w]);
+        this._worldMatrix = this._quat.toMat4(this.worldMatrix.copy());
+    }
+    */
 }
