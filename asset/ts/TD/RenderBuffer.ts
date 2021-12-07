240b9ea21e9359829edfef5c9309c431b004457f
Sleeeeeepy <adjustedmin@gmail.com>
2021-12-07 19:24:54 +0900

    Make RenderBuffer unbindable.

--- asset/ts/TD/RenderBuffer.ts
+++ asset/ts/TD/RenderBuffer.ts
@@ -3,25 +3,27 @@ import {GL} from './GL.js'
 export class RenderBuffer {
     private gl: WebGLRenderingContext = GL.instance;
     private renderBuffer: WebGLRenderbuffer | null;
-    public constructor(internalFormat: number, width: number, height: number) {
+    public constructor() {
         this.renderBuffer = this.gl.createRenderbuffer();
         if (!this.renderBuffer) {
             throw new Error("cannot create framebuffer.");
         }
-        this.bind();
-        this.gl.renderbufferStorage(this.gl.RENDERBUFFER, internalFormat, width, height);
     }
 
     public bind() {
         this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, this.renderBuffer);
     }
 
+    public unbind() {
+        this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, null);
+    }
+
     public storage(internalFormat: number, width: number, height: number) {
-        this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, this.renderBuffer);
+        this.bind();
         this.gl.renderbufferStorage(this.gl.RENDERBUFFER, internalFormat, width, height);
     }
 
-    public get GLRenderBuffer() {
+    public getWebGLRenderBuffer() {
         return this.renderBuffer;
     }
 }
